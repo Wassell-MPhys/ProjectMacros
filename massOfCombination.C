ebae6324e9a8c79d268876873580f3ef5b13d07d
Wassell-MPhysls <david.a.wassell@gmail.com>
2020-11-10 20:52:58 +0000

    macro now taking user input

--- massOfCombination.C
+++ massOfCombination.C
@@ -12,77 +12,76 @@ using namespace std;
 void massOfCombination() {
 
     // Data input assigned to variables
-    /*
     string dataSet;
-    string branchName1;
-    string branchName2;
+    string branch1;
+    string branch2;
     int bins;
     float xLower;
     float xUpper;
     cout << "Data Set? ";
     cin >> dataSet;
     cout << "First branch? ";
-    cin >> branchName1;
+    cin >> branch1;
     cout << "Second branch? ";
-    cin >> branchName2;
+    cin >> branch2;
     cout << "Number of bins? ";
     cin >> bins;
     cout << "From? ";
     cin >> xLower;
     cout << "To? ";
     cin >> xUpper;
-    */
 
     // Custom file location string from input data
-    // string filePath = "/run/media/davidwassell/David USB/" + dataSet + "/Lb_Tuple.root";
+    string filePath = "/run/media/davidwassell/David USB/" + dataSet + "/Lb_Tuple.root";
 
     // TFile* rootFile = TFile::Open(filePath.c_str());
-    TFile* rootFile = TFile::Open("/run/media/davidwassell/David USB/dataMagUp2016/Lb_Tuple.root");
+    TFile* rootFile = TFile::Open(filePath.c_str());
     TTree* tree = (TTree*) rootFile->Get("tree");
 
     // Enabling energy and momentum branches
     tree->SetBranchStatus("*",0);
-    // Input 1
-    tree->SetBranchStatus("mu1_PE",1);
-    tree->SetBranchStatus("mu1_PX",1);
-    tree->SetBranchStatus("mu1_PY",1);
-    tree->SetBranchStatus("mu1_PZ",1);
-    // Input 2
-    tree->SetBranchStatus("h2_PE",1);
-    tree->SetBranchStatus("h2_PX",1);
-    tree->SetBranchStatus("h2_PY",1);
-    tree->SetBranchStatus("h2_PZ",1);
+    // Input 1 - Use branch1 = mu1 for testing
+    tree->SetBranchStatus((branch1+"_PE").c_str(),1);
+    tree->SetBranchStatus((branch1+"_PX").c_str(),1);
+    tree->SetBranchStatus((branch1+"_PY").c_str(),1);
+    tree->SetBranchStatus((branch1+"_PZ").c_str(),1);
+    // Input 2 - use branch2 = h2 for testing
+    tree->SetBranchStatus((branch2+"_PE").c_str(),1);
+    tree->SetBranchStatus((branch2+"_PX").c_str(),1);
+    tree->SetBranchStatus((branch2+"_PY").c_str(),1);
+    tree->SetBranchStatus((branch2+"_PZ").c_str(),1);
 
-    double input1PE, input1PX, input1PY, input1PZ, input2PE, input2PX, input2PY, input2PZ;
+    double branch1PE, branch1PX, branch1PY, branch1PZ, branch2PE, branch2PX, branch2PY, branch2PZ;
     // Input 1
-    tree->SetBranchAddress("mu1_PE",&input1PE);
-    tree->SetBranchAddress("mu1_PX",&input1PX);
-    tree->SetBranchAddress("mu1_PY",&input1PY);
-    tree->SetBranchAddress("mu1_PZ",&input1PZ);  
+    tree->SetBranchAddress((branch1+"_PE").c_str(),&branch1PE);
+    tree->SetBranchAddress((branch1+"_PX").c_str(),&branch1PX);
+    tree->SetBranchAddress((branch1+"_PY").c_str(),&branch1PY);
+    tree->SetBranchAddress((branch1+"_PZ").c_str(),&branch1PZ);  
     // Input 2
-    tree->SetBranchAddress("h2_PE",&input2PE);
-    tree->SetBranchAddress("h2_PX",&input2PX);
-    tree->SetBranchAddress("h2_PY",&input2PY);
-    tree->SetBranchAddress("h2_PZ",&input2PZ);
+    tree->SetBranchAddress((branch2+"_PE").c_str(),&branch2PE);
+    tree->SetBranchAddress((branch2+"_PX").c_str(),&branch2PX);
+    tree->SetBranchAddress((branch2+"_PY").c_str(),&branch2PY);
+    tree->SetBranchAddress((branch2+"_PZ").c_str(),&branch2PZ);  
 
-    // Initialising histograms
-    TH1D* massCombinedHist = new TH1D("massCombinedHist","",200,0.,6000);
+    // Custom histogram title
+    string histTitle = "Mass Combination Histogram of " + branch1 + " and " + branch2; 
+    // Initialising histograms - for testing bins = 200, xLower = 0.0, xUpper = 6000
+    TH1D* massCombinedHist = new TH1D(histTitle.c_str(),"",bins,xLower,xUpper);
     // Error Bars
     // massCombinedHist->Sumw2(); 
 
     for ( Long64_t i = 0; i < tree->GetEntries(); i++ ){
         tree->GetEntry( i );
-        double momentumCombined = sqrt(pow((input1PX + input2PX), 2) + pow((input1PY + input2PY), 2) + pow((input1PZ + input2PZ), 2));
-        double energyCombined = input1PE + input2PE;
+        double momentumCombined = sqrt(pow((branch1PX + branch2PX), 2) + pow((branch1PY + branch2PY), 2) + pow((branch1PZ + branch2PZ), 2));
+        double energyCombined = branch1PE + branch2PE;
         double massCombined = sqrt(pow(energyCombined, 2) - pow(momentumCombined, 2));
         massCombinedHist->Fill(massCombined);
     }
   
-    TCanvas* massCombinedCanvas = new TCanvas("massCombinedCanvas","massCombinedCanvas");
+    TCanvas* massCombinedCanvas = new TCanvas(histTitle.c_str(),histTitle.c_str());
     massCombinedHist->Draw();
     massCombinedHist->SetYTitle("Entries");
     massCombinedHist->SetXTitle("Mass");
   return;
-
-
 }
+
