1e9ac7965b274b217e9a8fae272c043136b77146
David Wassell <davidwassell@localhost.localdomain>
2021-03-17 11:33:13 +0000

    p K J/Psi Lambda_b mass plot used in the investigation and references in the final report

--- LbMM.C
+++ LbMM.C
@@ -4,6 +4,8 @@
 #include "TTree.h"
 #include "TCanvas.h"
 #include "TLegend.h"
+#include "TLegend.h"
+#include "TStyle.h"
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -11,26 +13,24 @@
 using namespace std;
 
 void LbMM() {
-    // Data input assigned to variables
-    string dataSet, simSet, branchName;
-    int bins;
-    float xLower, xUpper;
-    cout << "Data Set? ";
-    cin >> dataSet;
-    cout << "Number of bins? ";
-    cin >> bins;
-    cout << "From? ";
-    cin >> xLower;
-    cout << "To? ";
-    cin >> xUpper;
-    
-    // Custom data file location string from input data
-    string dataFilePath = "/run/media/davidwassell/David USB/" + dataSet + "/Lb_Tuple.root";
+    // Setting plotting style
+    gStyle->SetTitleFont(132, "X");
+    gStyle->SetTitleFont(132, "Y");
+    gStyle->SetLabelFont(132, "X");
+    gStyle->SetLabelFont(132, "Y");
+    gStyle->SetTitleSize(0.04, "X");
+    gStyle->SetTitleSize(0.04, "Y");
+    gStyle->SetLabelSize(0.04, "X");
+    gStyle->SetLabelSize(0.04, "Y");
+    gStyle->SetLegendFont(132);  
+    int bins = 100;
+    float xLower = 5301.0;
+    float xUpper = 6001.0;
 
     // Open data file path and create a tree
-    TFile* rootDataFile = TFile::Open(dataFilePath.c_str());
-    TTree* tree = (TTree*) rootDataFile->Get("tree");
-    tree->AddFriend("Pred", "/run/media/davidwassell/David USB/Macros/Py_Macros/David MVA/Lb_Tuple-mva_bkg.root");
+    TFile* rootDataFile = TFile::Open("/run/media/davidwassell/David USB/dataMagDown2016/Lb2pKmm_mgDn_2016.root");
+    TTree* tree = (TTree*) rootDataFile->Get("Lb_Tuple/DecayTree");
+    tree->AddFriend("Pred", "/run/media/davidwassell/David USB/Macros/Py_Macros/David MVA/DMD2016/apply_output_DMD2016.root");
 
     // Custom Canvas title string from input
     string canvasTitle = "Canvas: Lb_MM Comparison"; 
@@ -39,37 +39,45 @@ void LbMM() {
     TCanvas* canvas = new TCanvas(canvasTitle.c_str(),canvasTitle.c_str()); 
 
     // 1st histogram for raw data - BLUE
-    TH1D* dataHist = new TH1D("dataHist","",bins,xLower,xUpper);
+    TH1D* dataHist = new TH1D("dataHist","dataHist",bins,xLower,xUpper);
     tree->Draw("Lb_MM>>dataHist");
     dataHist->SetLineColor( kBlue );
-    dataHist->SetLineWidth( 3 );
+    dataHist->SetLineWidth( 4 );
     dataHist->SetStats(0);
-    //dataHist->GetXaxis()->SetLabelSize(1.25);
-    //dataHist->GetYaxis()->SetLabelSize(1.25);
+    dataHist->SetMaximum(30000.0);
+    dataHist->SetMinimum(0.0);
 
-    // 2nd histogram with Lb_ENDBERTEX_CHI2 < 10 - RED
-    TH1D* dataHistCutByEye = new TH1D("dataHistCutByEye","",bins,xLower,xUpper);
+    // 2nd histogram with Visual Analysis Constraints - GREEN
+    TH1D* dataHistCutByEye = new TH1D("dataHistCutByEye","dataHistCutByEye",bins,xLower,xUpper);
     tree->Draw("Lb_MM>>dataHistCutByEye", "Lb_ENDVERTEX_CHI2<15 && Lb_PT>4500 && Lb_IPCHI2_OWNPV<4 && h1_PT>750 && h2_PT>750 && mu1_PT>750 && mu2_PT>750 && Lb_FDCHI2_OWNPV>500");
     // tree->Draw("Lb_MM>>dataHistCut", "MVA_S > 0.96");
     dataHistCutByEye->SetLineColor( kGreen+1 );
-    dataHistCutByEye->SetLineWidth( 3 );
-    // dataHistCutByEye->SetStats(0);
+    dataHistCutByEye->SetLineWidth( 4 );
+    dataHistCutByEye->SetStats(0);
     
-    // 2nd histogram with Lb_ENDBERTEX_CHI2 < 10 - RED
-    TH1D* dataHistCutByMacro = new TH1D("dataHistCutByMacro","",bins,xLower,xUpper);
+    // 2nd histogram with BDT Constraint - RED
+    TH1D* dataHistCutByMacro = new TH1D("dataHistCutByMacro","dataHistCutByMacro",bins,xLower,xUpper);
     // tree->Draw("Lb_MM>>dataHistCut", "Lb_ENDVERTEX_CHI2<15 && Lb_PT>4500 && Lb_IPCHI2_OWNPV<4 && h1_PT>750 && h2_PT>750 && mu1_PT>750 && mu2_PT>750 && Lb_FDCHI2_OWNPV>500");
-    tree->Draw("Lb_MM>>dataHistCutByMacro", "MVA_S > 0.96");
+    tree->Draw("Lb_MM>>dataHistCutByMacro", "MVA_S > 0.95");
     dataHistCutByMacro->SetLineColor( kRed );
-    dataHistCutByMacro->SetLineWidth( 3 );
-    // dataHistCutByMacro->SetStats(0);
+    dataHistCutByMacro->SetLineWidth( 4 );
+    dataHistCutByMacro->SetStats(0);
     
-    dataHist->Draw();
+    // Drawing Plots & setting axis labels
+    dataHist->Draw("");
     dataHist->SetTitle(" ");
-    dataHist->SetXTitle("LbMM (MeV/c^2)");
-    dataHist->SetYTitle("Counts");
+    dataHist->SetXTitle("#it{m}(#it{p}#it{K}^{#minus}#it{J}/#it{#Psi}) [MeV/#it{c}^{2}]");
+    dataHist->SetYTitle("Candidates / [7 MeV/#it{c}^{2}]");
     dataHistCutByEye->Draw("SAME");
     dataHistCutByMacro->Draw("SAME");
-    // dataHistCut2->Draw("SAME");
+
+
+    // Creating Legend
+    TLegend* leg = new TLegend();
+    leg->AddEntry("dataHist","Unconstrained","l");
+    leg->AddEntry("dataHistCutByEye","Visual Analysis Constraints","l");
+    leg->AddEntry("dataHistCutByMacro","BDT Constraints","l");
+    leg->Draw();
 
     return;
 }
